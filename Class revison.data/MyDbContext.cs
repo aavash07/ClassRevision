002acbb71eb26a0e65ef2e75b37e95817afc00c0
aavash07 <aavash07@users.noreply.github.com>
2021-02-18 16:30:06 +0545

    dbcontext

---
+++ Class revison.data/MyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Class_revison.data.Entinities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Class_revison.data
+{
+    class MyDbContext: DbContext
+    {
+        public MyDbContext(DbContextOpions<ClassDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<College> College { set; get; }
+
+        protected override void OnModeCreating(ModelBuildier modelBuilder)
+        {
+            modelBuilder.Entity<College>.ToTable("College");
+        }
+
+    }
+}
